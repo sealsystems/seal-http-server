f2c2c8a30280c959f45655e0f333a87546cf0dc2
Michael Scherer <michael.scherer@sealsystems.de>
2018-09-09 12:33:15 +0200

    feat: Use semantic-release

--- roboter.js
+++ roboter.js
@@ -2,15 +2,12 @@
 
 const roboter = require('roboter');
 
-// dummy require to avoid an unused dependencies error
-require('eslint-config-seal');
-
 roboter.
   workOn('server').
   equipWith((task) => {
     task('universal/analyze', {
       src: ['**/*.js', '!node_modules/**/*.js', '!examples/**', '!coverage/**', '!temp/**', '!output/**'],
-      rules: '.eslintrc'
+      rules: '.eslintrc.json'
     });
     task('universal/license', {
       disable: true
